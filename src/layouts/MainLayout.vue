a9bfec87b519086df1bb1bf2e8f550fe0f2ce673
DonPastor <marijan.pastor@gmail>
2022-09-20 23:50:38 +0200

    Page layout, l/r drawers, header, mobile header

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,116 +1,169 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
+    <q-layout view="lHr lpR fFf">
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
+    <q-header elevated class="bg-white text-black">
+        <q-toolbar>
+            <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
 
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
+            <q-toolbar-title class="text-weight-bold">
+                <span class="gt-sm">Qwitter</span>
+                <q-icon
+                  class="q-pa-md lt-md header-icon"
+                  size="sm"
+                  name="fa-solid fa-torii-gate"
+                  color="primary"
+                />
+            </q-toolbar-title>
+
+
+            </q-toolbar>
     </q-header>
 
     <q-drawer
-      v-model="leftDrawerOpen"
       show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
+      v-model="leftDrawerOpen"
+      side="left" bordered
+      >
+        <q-icon
+      class="q-pa-md"
+      size="lg"
+      name="fa-solid fa-torii-gate"
+      color="primary"
+    />
 
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
+    <q-list>
+      <q-item
+        clickable
+        v-ripple
+        to="/"
+        >
+        <q-item-section avatar>
+          <q-icon
+            name="home"
+            size="md"
+          />
+        </q-item-section>
+
+        <q-item-section
+          class="text-h6
+          text-weight-bold">Home</q-item-section>
+      </q-item>
+
+      <q-item
+        clickable
+        v-ripple
+        to="/about"
+        >
+        <q-item-section avatar>
+          <q-icon
+            name="support"
+            size="md"
+          />
+        </q-item-section>
+
+        <q-item-section
+          class="text-h6
+          text-weight-bold">About</q-item-section>
+        </q-item>
+
+    </q-list>
+
+    </q-drawer>
+
+    <q-drawer
+      show-if-above
+      v-model="rightDrawerOpen"
+      side="right"
+      bordered
+      >
+
+      <q-input
+        placeholder="Search Quitter"
+        class="q-ma-md"
+        outlined
+        rounded
+        dense
+        >
+        <template v-slot:prepend>
+          <q-icon name="search" />
+        </template>
+      </q-input>
+
+      <q-list padding separator>
+        <q-item class="q-pa-md">
+          <q-item-section>
+            <q-item-label overline class="text-grey" >Education</q-item-label>
+            <q-item-label class="text-weight-bold">Something amazing happen</q-item-label>
+            <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+          </q-item-section>
+
+          <q-item-section side top>
+            <q-item-label caption>5 min ago</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item class="q-pa-md">
+          <q-item-section>
+            <q-item-label overline class="text-grey" >Education</q-item-label>
+            <q-item-label class="text-weight-bold">Something amazing happen</q-item-label>
+            <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+          </q-item-section>
+
+          <q-item-section side top>
+            <q-item-label caption>5 min ago</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item class="q-pa-md">
+          <q-item-section>
+            <q-item-label overline class="text-grey" >Education</q-item-label>
+            <q-item-label class="text-weight-bold">Something amazing happen</q-item-label>
+            <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+          </q-item-section>
+
+          <q-item-section side top>
+            <q-item-label caption>5 min ago</q-item-label>
+          </q-item-section>
+        </q-item>
       </q-list>
+
     </q-drawer>
 
     <q-page-container>
-      <router-view />
+        <router-view />
     </q-page-container>
-  </q-layout>
+
+    </q-layout>
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
+import { ref } from 'vue'
 
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-]
+export default {
+    setup () {
+        const leftDrawerOpen = ref(false)
+        const rightDrawerOpen = ref(false)
 
-export default defineComponent({
-  name: 'MainLayout',
+        return {
+            leftDrawerOpen,
+                toggleLeftDrawer () {
+                leftDrawerOpen.value = !leftDrawerOpen.value
+                },
 
-  components: {
-    EssentialLink
-  },
-
-  setup () {
-    const leftDrawerOpen = ref(false)
-
-    return {
-      essentialLinks: linksList,
-      leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
+                rightDrawerOpen,
+                toggleRightDrawer () {
+                rightDrawerOpen.value = !rightDrawerOpen.value
+                }
+        }
     }
-  }
-})
+}
 </script>
+
+<style>
+  .header-icon {
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    transform: translate(-50%);
+  }
+</style>
